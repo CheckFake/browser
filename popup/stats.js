f9c5a4cd298a899a35839036d2435190b76af1ab
Gabriel Augendre <gabriel@augendre.info>
2018-10-06 13:04:44 +0200

    Use the API to compute scores instead of doing it locally

--- popup/stats.js
+++ popup/stats.js
@@ -1,98 +1,33 @@
-/**
- * Just log the error to the console.
- */
-function reportError(error) {
-    console.error(`Could not fetch page: ${error}`);
-}
-
-/**
- * Computes the confidence score given the page data.
- */
-function computeConfidenceScore(data) {
-    if (data.fromLocalStorage) {
-        return data;
-    }
-    data.scores = {
-        'website-score': {
-            value: 0,
-            title: 'Website',
-            type: 'details'
-        },
-        'author-score': {
-            value: 0,
-            title: 'Author',
-            type: 'details'
-        }
-    };
-    for (const key of Object.keys(data.scores)) {
-        data.scores[key].value = Math.round(Math.random() * 100);
-    }
-
-    let confidenceScore = Object.values(data.scores).reduce((a, b) => a.value + b.value);
-    data.scores['confidence-score'] = {
-        type: 'overall',
-        value: Math.round(confidenceScore / Object.keys(data.scores).length)
-    };
-    return data;
-}
-
 function displayConfidenceScore(data, tabs) {
     let tab = tabs[0];
 
-    document.querySelector("#page-name").innerText = data.tab.title;
-    document.querySelector('#confidence-score').innerText = data.scores['confidence-score'].value;
+    document.querySelector("#page-name").innerText = tab.title;
+    document.querySelector('#confidence-score').innerText = data.data.global_score;
     let tableScores = document.querySelector('#scores');
 
-    Object.entries(data.scores)
-        .filter(([key, score]) => score && score.type === 'details')
+    Object.entries(data.data.scores)
         .forEach(([key, score]) => {
             let line = document.createElement('tr');
             line.innerHTML = `<tr>
-                <th scope="row">${score.title}</th>
-                <td id="${key}" class="score">${score.value}</td>
+                <th scope="row">${key}</th>
+                <td id="${key}" class="score">${score}</td>
             </tr>`;
             tableScores.appendChild(line);
         });
 
     let badgeDetails = {
         tabId: tab.id,
-        text: data.scores['confidence-score'].value.toString()
+        text: data.data.global_score.toString()
     };
     return browser.browserAction.setBadgeText(badgeDetails);
 }
 
 /**
- * Attempts to retrieve data from local storage
+ * Queries the API for the scores of the current tab
  */
-function fetchDataFromStorage(tabs) {
-    let tab = tabs[0];
-    return browser.storage.local.get(tab.url);
-}
-
-/**
- * Stores data in the local storage
- */
-function storeData([tabs, data]) {
-    if (data.fromLocalStorage) {
-        return;
-    }
-    let tab = tabs[0];
-    let contentToStore = {};
-    contentToStore[tab.url] = data;
-    return browser.storage.local.set(contentToStore);
-}
-
-/**
- * Queries the content script.
- */
-function fetchTabData([tabs, localData]) {
-    let tab = tabs[0];
-    if (localData && Object.keys(localData).length > 0) {
-        let data = localData[tab.url];
-        data.fromLocalStorage = true;
-        return data;
-    }
-    return browser.tabs.sendMessage(tab.id, {command: "fetchData"});
+function queryAPI(tabs) {
+    let url = encodeURIComponent(tabs[0].url);
+    return fetch(`https://fakenewsdetector.augendre.info/api/page?url=${url}`);
 }
 
 /**
@@ -123,47 +58,28 @@ function toggleScoreDetails() {
     document.querySelector("#confidence-score-wrapper").addEventListener('click', toggleScoreDetails);
 })();
 
-let script = {file: "/content_scripts/get_page_data.js"};
 let backgroundColorDetails = {
     color: "#FFD729"
 };
 
-browser.tabs.executeScript({file: "/libs/browser-polyfill.min.js"})
+
+let activeTabPromise = getActiveTab()
     .catch(error => {
         showErrorInPopup();
-        console.error("Error injecting polyfill script", error);
+        console.error("Error getting active tab", error);
     });
 
-let activeTabPromise = browser.tabs.executeScript(script)
-    .then(getActiveTab)
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error injecting extension script or retrieving active tab", error);
-    });
-
-let fetchFromStoragePromise = activeTabPromise
-    .then(fetchDataFromStorage)
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error fetching from storage", error);
-    });
-
-let computeScorePromise = Promise.all([activeTabPromise, fetchFromStoragePromise])
-    .then(fetchTabData)
-    .then(computeConfidenceScore)
+let getScorePromise = activeTabPromise
+    .then(queryAPI)
+    .then(response => {
+        return response.json();
+    })
     .catch(error => {
         showErrorInPopup();
         console.error("Error computing score", error);
     });
 
-Promise.all([activeTabPromise, computeScorePromise])
-    .then(storeData)
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error storing data", error);
-    });
-
-Promise.all([activeTabPromise, computeScorePromise, browser.browserAction.setBadgeBackgroundColor(backgroundColorDetails)])
+Promise.all([activeTabPromise, getScorePromise, browser.browserAction.setBadgeBackgroundColor(backgroundColorDetails)])
     .then(([tabs, data]) => displayConfidenceScore(data, tabs))
     .catch(error => {
         showErrorInPopup();
