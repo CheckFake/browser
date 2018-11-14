59aa3bc1dc3394385b4f1206841ab4f1c4192d31
Gabriel Augendre <gabriel@augendre.info>
2018-11-14 12:09:58 +0100

    Run npm production script when generating zip file

--- zip_prod.sh
+++ zip_prod.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf fake_news_detector.zip
+npm run production
 zip -r fake_news_detector.zip background_scripts icons/logo.svg icons/logo@2x.png icons/logo_dev.svg libs images popup/libs popup/stats.css popup/stats.html popup/dist manifest.json
