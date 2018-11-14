5f5b704db3002048660f64eb523df4c2466fa032
Gabriel Augendre <gabriel@augendre.info>
2018-11-14 11:52:27 +0100

    Use vue.js

---
+++ popup/app.vue
@@ -0,0 +1,173 @@
+<template>
+    <div class="container-fluid">
+        <div>
+            <div class="row">
+                <h1 class="col-12 popup-title">
+                    <img src="../icons/logo.svg" height="48" width="48" alt="Logo"> OnlyTrue
+                </h1>
+            </div>
+            <hr>
+        </div>
+        <div id="popup-content">
+            <div class="row">
+                <div class="col-12">
+                    <strong>Page :</strong> {{ page.url }}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <strong>Author :</strong> {{ page.author }}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <span class="confidence-score">{{ confidenceScore }}</span>%
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <meter min="0" low="50" high="80" optimum="100" max="100" v-bind:value="confidenceScore" title="%"
+                           id="confidence-meter"></meter>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-12">
+                    <h4>Related articles</h4>
+                    <ul id="related-articles">
+                        <li v-for="article in relatedArticles"><a v-bind:href="article.url">{{ article.publisher }} - {{ article.title }}</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div id="score-details" class="row">
+                <div class="col-12">
+                    <h4>Scoring details</h4>
+                    <p>
+                        The page was given a confidence score of
+                        <span class="emphasize">{{ confidenceScore }}%</span>.<br>
+                        Here are the detailed scores :
+                    </p>
+                    <div class="table-responsive">
+                        <table class="table table-sm table-hover table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">Item</th>
+                                <th scope="col">Score</th>
+                            </tr>
+                            </thead>
+                            <tbody id="scores">
+                            <tr v-for="(score, key) in scores">
+                                <th scope="row">{{ getItemNameFromKey(key) }}</th>
+                                <td v-bind:id="key" class="score">{{ score }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="error-content" class="hidden">
+            Couldn't fetch page data.
+        </div>
+        <div class="feedback">
+            <a href="https://github.com/Crocmagnon/fake-news-detector/issues/new">Give feedback</a>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                page: {
+                    url: null,
+                    author: null
+                },
+                confidenceScore: null,
+                relatedArticles: [],
+                scores: []
+            }
+        },
+        methods: {
+            getItemNameFromKey(key) {
+                let arr = key.split("_");
+                arr.pop();
+                arr = arr.join(" ");
+                return this.capitalizeFirstLetter(arr);
+            },
+            capitalizeFirstLetter(str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            },
+            displayConfidenceScore(data, tabs) {
+                let tab = tabs[0];
+
+                this.page.url = tab.url;
+                this.page.author = "Unknown";
+                this.confidenceScore = data.data.global_score;
+
+                this.scores = data.data.scores;
+                this.relatedArticles = data.data.related_articles_selection;
+
+                let badgeDetails = {
+                    tabId: tab.id,
+                    text: data.data.global_score.toString()
+                };
+                return browser.browserAction.setBadgeText(badgeDetails);
+            }
+        },
+        created() {
+            /**
+             * Queries the API for the scores of the current tab
+             */
+            function queryAPI(tabs) {
+                let url = encodeURIComponent(tabs[0].url);
+                return fetch(`https://fakenewsdetector.augendre.info/api/page?url=${url}`);
+            }
+
+            /**
+             * Queries browser for the active tab.
+             */
+            function getActiveTab() {
+                let tabQuery = {active: true, currentWindow: true};
+                return browser.tabs.query(tabQuery);
+            }
+
+            /**
+             * There was an error executing the script.
+             * Display the popup's error message, and hide the normal UI.
+             */
+            function showErrorInPopup() {
+                document.querySelector("#popup-content").classList.add("hidden");
+                document.querySelector("#error-content").classList.remove("hidden");
+            }
+
+            let backgroundColorDetails = {
+                color: "#FFD729"
+            };
+
+            let activeTabPromise = getActiveTab()
+                .catch(error => {
+                    showErrorInPopup();
+                    console.error("Error getting active tab", error);
+                });
+
+            let getScorePromise = activeTabPromise
+                .then(queryAPI)
+                .then(response => {
+                    return response.json();
+                })
+                .catch(error => {
+                    showErrorInPopup();
+                    console.error("Error computing score", error);
+                });
+
+            Promise.all([activeTabPromise, getScorePromise, browser.browserAction.setBadgeBackgroundColor(backgroundColorDetails)])
+                .then(([tabs, data]) => this.displayConfidenceScore(data, tabs))
+                .catch(error => {
+                    showErrorInPopup();
+                    console.error("Error displaying score", error);
+                });
+
+        }
+    }
+</script>
--- popup/stats.js
+++ popup/stats.js
@@ -1,101 +1,12 @@
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+import Vue from 'vue';
+import App from './app.vue'
 
-function getItemNameFromKey(key) {
-    let arr = key.split("_");
-    arr.pop();
-    arr = arr.join(" ");
-    return capitalizeFirstLetter(arr);
-}
-
-function displayConfidenceScore(data, tabs) {
-    let tab = tabs[0];
-
-    for (let item of document.querySelectorAll(".page-url")) {
-        item.innerText = tab.url;
+let vm = new Vue({
+    el: '#vue-app',
+    components: {
+        App
+    },
+    render(h) {
+        return h('app');
     }
-    for (let item of document.querySelectorAll(".page-author")) {
-        item.innerText = "Unknown";
-    }
-    for (let item of document.querySelectorAll(".confidence-score")) {
-        item.innerText = data.data.global_score;
-    }
-    document.querySelector('#confidence-meter').setAttribute("value", data.data.global_score);
-
-    let tableScores = document.querySelector('#scores');
-    Object.entries(data.data.scores)
-        .forEach(([key, score]) => {
-            let line = document.createElement('tr');
-            line.innerHTML = `<tr>
-                <th scope="row">${getItemNameFromKey(key)}</th>
-                <td id="${key}" class="score">${score}</td>
-            </tr>`;
-            tableScores.appendChild(line);
-        });
-    let relatedArticlesList = document.querySelector('#related-articles');
-    for (let relatedArticle of data.data.related_articles_selection) {
-        let item = document.createElement('li');
-        item.innerHTML = `<li><a href="${relatedArticle.url}">${relatedArticle.publisher} - ${relatedArticle.title}</a></li>`;
-        relatedArticlesList.appendChild(item);
-    }
-
-    let badgeDetails = {
-        tabId: tab.id,
-        text: data.data.global_score.toString()
-    };
-    return browser.browserAction.setBadgeText(badgeDetails);
-}
-
-/**
- * Queries the API for the scores of the current tab
- */
-function queryAPI(tabs) {
-    let url = encodeURIComponent(tabs[0].url);
-    return fetch(`https://fakenewsdetector.augendre.info/api/page?url=${url}`);
-}
-
-/**
- * Queries browser for the active tab.
- */
-function getActiveTab() {
-    let tabQuery = {active: true, currentWindow: true};
-    return browser.tabs.query(tabQuery);
-}
-
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
-function showErrorInPopup() {
-    document.querySelector("#popup-content").classList.add("hidden");
-    document.querySelector("#error-content").classList.remove("hidden");
-}
-
-let backgroundColorDetails = {
-    color: "#FFD729"
-};
-
-
-let activeTabPromise = getActiveTab()
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error getting active tab", error);
-    });
-
-let getScorePromise = activeTabPromise
-    .then(queryAPI)
-    .then(response => {
-        return response.json();
-    })
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error computing score", error);
-    });
-
-Promise.all([activeTabPromise, getScorePromise, browser.browserAction.setBadgeBackgroundColor(backgroundColorDetails)])
-    .then(([tabs, data]) => displayConfidenceScore(data, tabs))
-    .catch(error => {
-        showErrorInPopup();
-        console.error("Error displaying score", error);
-    });
+});
