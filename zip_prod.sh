4e7957e9bf120682c56d2eade4c29a76cb509244
Gabriel Augendre <gabriel@augendre.info>
2018-11-14 12:08:13 +0100

    Add scripts to generate source and distribution zip files

---
+++ zip_prod.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf fake_news_detector.zip
+zip -r fake_news_detector.zip background_scripts icons/logo.svg icons/logo@2x.png icons/logo_dev.svg libs images popup/libs popup/stats.css popup/stats.html popup/dist manifest.json
---
+++ zip_sources.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf fake_news_detector_src.zip
+git archive HEAD -o fake_news_detector_src.zip
