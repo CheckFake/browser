3c57484af39c82accf77103ffca5bbfe496c3415
Gabriel Augendre <gabriel@augendre.info>
2018-12-04 15:09:19 +0100

    Add loading screen

--- popup/app.vue
+++ popup/app.vue
@@ -1,90 +1,110 @@
 <template>
     <div class="container-fluid">
-        <div>
-            <div class="row">
-                <h1 class="col-12 popup-title">
-                    <img src="../icons/logo.svg" height="48" width="48" alt="Logo"> OnlyTrue
-                </h1>
-            </div>
-            <hr>
+        <div v-if="loading">
+            Loading...
         </div>
-        <div id="error-content" v-if="errors.length > 0">
-            <h4>Errors</h4>
-            <p>Les erreurs seront probablement affichées en anglais.
-                Merci de nous contacter via le lien ci-dessous si vous rencontrez une erreur
-                alors que vous pensiez obtenir un résultat.</p>
-            <ul class="list">
-                <li v-for="error in errors">{{ error }}</li>
-            </ul>
-        </div>
-        <div id="popup-content" v-else>
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h4>Niveau de confiance</h4>
-                    <p>
-                        <strong>Titre :</strong> {{ page.title }}
-                    </p>
-                    <span title="Très peu fiable" class="confidence-score-box score-box" v-bind:class="colorClassFromScore(this.confidenceScore, 'bad')">1</span>
-                    <span title="Peu fiable" class="confidence-score-box score-box" v-bind:class="colorClassFromScore(this.confidenceScore, 'not-so-bad')">2</span>
-                    <span title="Contenu incertain" class="confidence-score-box score-box" v-bind:class="colorClassFromScore(this.confidenceScore, 'meh')">3</span>
-                    <span title="Fiable" class="confidence-score-box score-box" v-bind:class="colorClassFromScore(this.confidenceScore, 'not-so-good')">4</span>
-                    <span title="Très fiable" class="confidence-score-box score-box" v-bind:class="colorClassFromScore(this.confidenceScore, 'good')">5</span>
+        <div v-if="!loading">
+            <div>
+                <div class="row">
+                    <h1 class="col-12 popup-title">
+                        <img src="../icons/logo.svg" height="48" width="48" alt="Logo"> OnlyTrue
+                    </h1>
                 </div>
+                <hr>
             </div>
-            <hr>
-            <div class="row">
-                <div class="col-12">
-                    <h4>Articles connexes</h4>
-                    <p>
-                        {{ interestingRelatedArticlesCount }}
-                        {{ pluralize("article", interestingRelatedArticlesCount) }} parmi ceux que nous avons analysés
-                        {{ pluralize("est", interestingRelatedArticlesCount, "sont", true) }}
-                        {{ pluralize("similaire", interestingRelatedArticlesCount) }}
-                        à votre article. En voici {{ relatedArticles.length }} :
-                    </p>
-                    <ul class="list">
-                        <li v-for="article in relatedArticles"><a v-bind:href="article.url">{{ article.publisher }} - {{ article.title }}</a></li>
-                    </ul>
+            <div id="error-content" v-if="errors.length > 0">
+                <h4>Errors</h4>
+                <p>Les erreurs seront probablement affichées en anglais.
+                    Merci de nous contacter via le lien ci-dessous si vous rencontrez une erreur
+                    alors que vous pensiez obtenir un résultat.</p>
+                <ul class="list">
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </div>
+            <div id="popup-content" v-else>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h4>Niveau de confiance</h4>
+                        <p>
+                            <strong>Titre :</strong> {{ page.title }}
+                        </p>
+                        <span title="Très peu fiable" class="confidence-score-box score-box"
+                              v-bind:class="colorClassFromScore(this.confidenceScore, 'bad')">1</span>
+                        <span title="Peu fiable" class="confidence-score-box score-box"
+                              v-bind:class="colorClassFromScore(this.confidenceScore, 'not-so-bad')">2</span>
+                        <span title="Contenu incertain" class="confidence-score-box score-box"
+                              v-bind:class="colorClassFromScore(this.confidenceScore, 'meh')">3</span>
+                        <span title="Fiable" class="confidence-score-box score-box"
+                              v-bind:class="colorClassFromScore(this.confidenceScore, 'not-so-good')">4</span>
+                        <span title="Très fiable" class="confidence-score-box score-box"
+                              v-bind:class="colorClassFromScore(this.confidenceScore, 'good')">5</span>
+                    </div>
                 </div>
-            </div>
-            <hr>
-            <button type="button" class="btn btn-outline-secondary btn-sm" v-on:click="details = true" v-if="!details">Plus de détails</button>
-            <div id="score-details" v-bind:class="this.details ? 'row' : 'hidden'">
-                <div class="col-12">
-                    <h4>Détails de la notation</h4>
-                    <p>
-                        Le score attribué <strong>au contenu</strong> a été calculé en analysant
-                        <strong>{{ totalArticles }}</strong>
-                        {{ pluralize("autre", totalArticles) }}
-                        {{ pluralize("article", totalArticles) }}.<br>
-                        Le score attribué <strong>au site</strong> a été calculé par l'analyse précédente de
-                        <strong>{{ siteScoreArticlesCount - 1 }}</strong>
-                        {{ pluralize("autre", siteScoreArticlesCount) }}
-                        {{ pluralize("article", siteScoreArticlesCount) }}.<br>
-                        <a href="https://github.com/OnlyTrue-FND/api/wiki/Calcul-du-score">Plus de détails sur la méthode de calcul</a><br>
-                        Voici le détail des scores :
-                    </p>
-                    <div class="table-responsive">
-                        <table class="table table-sm table-hover table-bordered">
-                            <thead>
-                            <tr>
-                                <th scope="col">Élément</th>
-                                <th scope="col">Score</th>
-                            </tr>
-                            </thead>
-                            <tbody id="scores">
-                            <tr v-for="(score, key) in scores">
-                                <th scope="row">{{ getItemNameFromKey(key) }}</th>
-                                <td v-bind:id="key" class="score">
-                                    <span title="Très peu fiable" class="details-score-box score-box" v-bind:class="colorClassFromScore(score, 'bad')">1</span>
-                                    <span title="Peu fiable" class="details-score-box score-box" v-bind:class="colorClassFromScore(score, 'not-so-bad')">2</span>
-                                    <span title="Contenu incertain" class="details-score-box score-box" v-bind:class="colorClassFromScore(score, 'meh')">3</span>
-                                    <span title="Fiable" class="details-score-box score-box" v-bind:class="colorClassFromScore(score, 'not-so-good')">4</span>
-                                    <span title="Très fiable" class="details-score-box score-box" v-bind:class="colorClassFromScore(score, 'good')">5</span>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
+                <hr>
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Articles connexes</h4>
+                        <p>
+                            {{ interestingRelatedArticlesCount }}
+                            {{ pluralize("article", interestingRelatedArticlesCount) }} parmi ceux que nous avons
+                            analysés
+                            {{ pluralize("est", interestingRelatedArticlesCount, "sont", true) }}
+                            {{ pluralize("similaire", interestingRelatedArticlesCount) }}
+                            à votre article. En voici {{ relatedArticles.length }} :
+                        </p>
+                        <ul class="list">
+                            <li v-for="article in relatedArticles"><a v-bind:href="article.url">{{ article.publisher }}
+                                - {{ article.title }}</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <button type="button" class="btn btn-outline-secondary btn-sm" v-on:click="details = true"
+                        v-if="!details">Plus de détails
+                </button>
+                <div id="score-details" v-bind:class="this.details ? 'row' : 'hidden'">
+                    <div class="col-12">
+                        <h4>Détails de la notation</h4>
+                        <p>
+                            Le score attribué <strong>au contenu</strong> a été calculé en analysant
+                            <strong>{{ totalArticles }}</strong>
+                            {{ pluralize("autre", totalArticles) }}
+                            {{ pluralize("article", totalArticles) }}.<br>
+                            Le score attribué <strong>au site</strong> a été calculé par l'analyse précédente de
+                            <strong>{{ siteScoreArticlesCount - 1 }}</strong>
+                            {{ pluralize("autre", siteScoreArticlesCount) }}
+                            {{ pluralize("article", siteScoreArticlesCount) }}.<br>
+                            <a href="https://github.com/OnlyTrue-FND/api/wiki/Calcul-du-score">Plus de détails sur la
+                                méthode de calcul</a><br>
+                            Voici le détail des scores :
+                        </p>
+                        <div class="table-responsive">
+                            <table class="table table-sm table-hover table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Élément</th>
+                                    <th scope="col">Score</th>
+                                </tr>
+                                </thead>
+                                <tbody id="scores">
+                                <tr v-for="(score, key) in scores">
+                                    <th scope="row">{{ getItemNameFromKey(key) }}</th>
+                                    <td v-bind:id="key" class="score">
+                                        <span title="Très peu fiable" class="details-score-box score-box"
+                                              v-bind:class="colorClassFromScore(score, 'bad')">1</span>
+                                        <span title="Peu fiable" class="details-score-box score-box"
+                                              v-bind:class="colorClassFromScore(score, 'not-so-bad')">2</span>
+                                        <span title="Contenu incertain" class="details-score-box score-box"
+                                              v-bind:class="colorClassFromScore(score, 'meh')">3</span>
+                                        <span title="Fiable" class="details-score-box score-box"
+                                              v-bind:class="colorClassFromScore(score, 'not-so-good')">4</span>
+                                        <span title="Très fiable" class="details-score-box score-box"
+                                              v-bind:class="colorClassFromScore(score, 'good')">5</span>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -108,7 +128,8 @@
                 siteScoreArticlesCount: null,
                 interestingRelatedArticlesCount: null,
                 errors: [],
-                details: false
+                details: false,
+                loading: true
             }
         },
         methods: {
@@ -139,24 +160,19 @@
              * @param {string, null} pastilleColor
              * @returns {string}
              */
-            colorClassFromScore(score, pastilleColor=null) {
+            colorClassFromScore(score, pastilleColor = null) {
                 let cls = null;
                 if (score >= 80) {
                     cls = "good"
-                }
-                else if (score >= 60) {
+                } else if (score >= 60) {
                     cls = "not-so-good"
-                }
-                else if (score >= 40) {
+                } else if (score >= 40) {
                     cls = "meh"
-                }
-                else if (score >= 20) {
+                } else if (score >= 20) {
                     cls = "not-so-bad"
-                }
-                else if (score >= 0) {
+                } else if (score >= 0) {
                     cls = "bad"
-                }
-                else {
+                } else {
                     cls = null;
                 }
                 if (!pastilleColor) {
@@ -184,6 +200,7 @@
                 return cls
             },
             displayConfidenceScore(data, tabs) {
+                this.loading = false;
                 let tab = tabs[0];
                 this.page.title = tab.title;
                 this.confidenceScore = data.data.global_score;
@@ -206,14 +223,14 @@
                 };
             },
             displayError(message, error) {
+                this.loading = false;
                 this.errors.push(`${message} : ${error}`);
                 console.error(message, error);
             },
-            pluralize(word, count, suffix='s', replace=false) {
+            pluralize(word, count, suffix = 's', replace = false) {
                 if (replace && count > 1) {
                     return suffix
-                }
-                else if (count > 1) {
+                } else if (count > 1) {
                     return word + suffix;
                 }
                 return word
