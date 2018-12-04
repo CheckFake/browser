ab55dab256600ca7356e74672e07eeb5cc9de04c
Gabriel Augendre <gabriel@augendre.info>
2018-12-04 15:23:05 +0100

    Display a waiting animation

--- popup/app.vue
+++ popup/app.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="container-fluid">
+        <div>
+            <div class="row">
+                <h1 class="col-12 popup-title">
+                    <img src="../icons/logo.svg" height="48" width="48" alt="Logo"> OnlyTrue
+                </h1>
+            </div>
+            <hr>
+        </div>
         <div v-if="loading">
-            Loading...
+            <div class="sk-cube-grid">
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube5"></div>
+                <div class="sk-cube sk-cube6"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube8"></div>
+                <div class="sk-cube sk-cube9"></div>
+            </div>
         </div>
         <div v-if="!loading">
-            <div>
-                <div class="row">
-                    <h1 class="col-12 popup-title">
-                        <img src="../icons/logo.svg" height="48" width="48" alt="Logo"> OnlyTrue
-                    </h1>
-                </div>
-                <hr>
-            </div>
             <div id="error-content" v-if="errors.length > 0">
                 <h4>Errors</h4>
                 <p>Les erreurs seront probablement affichées en anglais.
